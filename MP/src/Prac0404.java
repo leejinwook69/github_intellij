ee0fc71ab3bcd05a4a8e67a2f8fc15627d2494ac
leejinwook69 <leejinwook69@gmail.com>
2021-04-04 23:02:49 +0900

    prac

---
+++ MP/src/Prac0404.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Prac0404 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String in = "";
+        while (in.compareTo("quit") != 0) {
+            System.out.println("문자열을 입력하세요. 그만하려면 quit을 입력하세요. : \n");
+            in = sc.next();
+            System.out.printf("%s\n", in);
+        }
+    }
+
+
+}
